9a37c8b5a0faf0158c8fc56092f580e2941553d2
verden11 <justinas.reigis@gmail.com>
2015-12-26 01:20:46 +0000

    tablet mode layout added

--- app/src/main/java/com/verden1/connect3/MainActivity.java
+++ app/src/main/java/com/verden1/connect3/MainActivity.java
@@ -12,15 +12,17 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView winnerMessage;
     boolean gameIsActive = true;
-
     // 0 = blue, 1 = red
     int activePlayer = 0;
-
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2}; //2 - unplayed
-
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},    //rows
-            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},    // columns
-            {0, 4, 8}, {2, 4, 6}};              // diagonals
+                                {0, 3, 6}, {1, 4, 7}, {2, 5, 8},    // columns
+                                {0, 4, 8}, {2, 4, 6}};              // diagonals
+
+
+
+
+
 
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
